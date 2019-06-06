6e65a3eb2c6624f5bffb6e8250edaba52b67d8cb
Leonardo Sena <leeosena21@gmail.com>
2019-06-06 00:16:58 -0300

    Lendo e validando arquivo .191

---
+++ Compiladores/Lexico.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Compiladores
+{
+
+    public class Lexico
+    {
+        public String Matricula;
+        public String Nome;
+        public float Salario;
+        public int Dia;
+        public int Mes;
+        public int Ano;
+        public String Setor;
+
+        public Lexico(String line)
+        {
+
+            getMatricula(line);
+            getNome(line);
+            getSalario(line);
+            getDia(line);
+            getMes(line);
+            getAno(line);
+            getSetor(line);
+        }
+
+        public void getMatricula(String line)
+        {
+            String[] substrings = line.Split(';');
+            Matricula = substrings[0];
+            Console.WriteLine(Matricula);
+        }
+
+        public void getNome(String line)
+        {
+            String[] substrings = line.Split(';');
+            Nome = substrings[1];
+            Console.WriteLine(Nome);
+        }
+
+        public void getSalario(String line)
+        {
+            String[] substrings = line.Split(';');
+            Salario = float.Parse(substrings[2]);
+            Console.WriteLine(Salario);
+        }
+
+        public void getDia(String line)
+        {
+            String[] substrings = line.Split(';');
+            Dia = int.Parse(substrings[3]);
+            Console.WriteLine(Dia);
+        }
+        public void getMes(String line)
+        {
+            String[] substrings = line.Split(';');
+            Mes = int.Parse(substrings[4]);
+            Console.WriteLine(Mes);
+        }
+        public void getAno(String line)
+        {
+            String[] substrings = line.Split(';');
+            Ano = int.Parse(substrings[5]);
+            Console.WriteLine(Ano);
+        }
+        public void getSetor(String line)
+        {
+            String[] substrings = line.Split(';');
+            Setor = substrings[6];
+            Console.WriteLine(Setor);
+        }
+
+    }
+}
+
---
+++ Compiladores/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Compiladores
+{
+    public class Program
+    {
+        public static string path;
+        public static string[] readText;
+        static void Main(string[] args)
+        {
+
+            List<Lexico> Func = new List<Lexico>();
+
+            Console.WriteLine("Digite ação:\n1-Informar nome do arquivo\n2-Path do arquivo");
+            string resp = Console.ReadKey().KeyChar.ToString();
+
+            switch (resp)
+            {
+                case "1":
+                    Console.WriteLine("\nInforme nome do arquivo: ");
+                    resp = Console.ReadLine().ToString();
+                    path = $@"{Directory.GetCurrentDirectory()}/{resp}.191";
+                    break;
+
+                case "2":
+                    Console.WriteLine("\nInforme path do arquivo: ");
+                    resp = Console.ReadLine().ToString();
+                    path = $@"resp";
+                    break;
+
+
+            }
+            if (FileExist(path))
+                readText = File.ReadAllLines(path);
+            else
+            {
+                Console.WriteLine("\nDiretorio não encontrado, reinicie a aplicação!");
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
+
+
+            Console.ReadKey();
+
+        }
+
+
+        public static bool FileExist(String Path)
+        {
+            if (File.Exists(Path))
+                return true;
+            else
+                return false;
+        }
+    }
+}
+
